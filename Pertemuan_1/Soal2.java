a9b99795c92e9545dc76592616fa63c3d8ec6866
Sulthan <sulthan.aulia.tif23@polban.ac.id>
2024-08-27 17:51:25 +0700

    Post ke git hub Pertemuan_1 :rocket:

---
+++ Pertemuan_1/Soal2.java
@@ -0,0 +1,17 @@
+public class Soal2 {
+  public static void main(String[] args) {
+    int i = 42;
+    String s=(i<40)?"life":(i>50)?"universe":"everything";
+    System.out.println(s);
+  }
+}
+/*
+if (i<40)
+{
+print("life")
+}else if(i>50){
+print("universe")
+}else
+{
+print("everything")}
+*/
